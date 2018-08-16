4274559dd72b2435099be5adf600763fb1b6b4fc
Akshay Sethi <asethi09@in.ibm.com>
2018-08-16 18:10:11 +0530

    added rnn model

---
+++ code.py
@@ -0,0 +1,13 @@
+
+from keras.layers import Input
+from keras.layers import Dense
+from keras.layers.recurrent import LSTM
+
+visible = Input(shape=(100,1))
+hidden1 = LSTM(10)(visible)
+hidden2 = Dense(10, activation='relu')(hidden1)
+output = Dense(1, activation='sigmoid')(hidden2)
+model = Model(inputs=visible, outputs=output)
+
+print(model.summary())
+
